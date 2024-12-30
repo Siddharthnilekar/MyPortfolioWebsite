0830273cd7b75d30a1c18e2b913bcf824b7df0e9
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2024-12-31 01:29:29 +0530

    tilt erropr

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // This splits node_modules into separate chunks
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  }
-});
-
+  plugins: [react()],
+})
